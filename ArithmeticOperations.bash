9ba0cf24ffa1b8915543ee46b15414418d9e1c46
vinayg22 <48248081+vinayg22@users.noreply.github.com>
2022-11-23 19:42:58 +0530

    Arithmetic Operations
    A mathematical expression containing +,-,*,^, / and parenthesis will be provided. Read in the expression, then evaluate it. Display the result rounded to 3 decimal places.

---
+++ ArithmeticOperations.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+<<com
+A mathematical expression containing +,-,*,^, / and parenthesis will be provided. Read in the expression, then evaluate it. Display the result rounded to  decimal places.
+
+Constraints
+
+All numeric values are <= 999.
+
+Sample Input
+
+Sample Input 1
+
+5+50*3/20 + (19*2)/7
+Sample Input 2
+
+-105+50*3/20 + (19^2)/7
+Sample Input 3
+
+(-105.5*7+50*3)/20 + (19^2)/7
+Sample Output
+
+Sample Output 1
+
+17.929
+Sample Output 2
+
+-45.929
+Sample Output 3
+
+ 22.146
+ com
+ 
+ read input
+printf "%.3f\n" $( bc -l <<< $input )
