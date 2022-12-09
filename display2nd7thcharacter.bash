48618b910d2eb835d4b7d6b57ea51140b585dd79
vinayg22 <48248081+vinayg22@users.noreply.github.com>
2022-12-09 15:37:47 +0530

    Cut #2
    Each line should contain just two characters at the 3nd and the 7th position of the corresponding input line

---
+++ display2nd7thcharacter.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+Display the  and  character from each line of text.
+
+Input Format
+
+A text file with  lines of ASCII text only.
+
+Constraints
+
+Output Format
+
+The output should contain  lines. Each line should contain just two characters at the  and the  position of the corresponding input line.
+
+Sample Input
+
+Hello
+
+World
+
+how are you
+
+Sample Output
+
+e
+
+o
+
+oe
+
+cut -c2,7 /dev/stdin
